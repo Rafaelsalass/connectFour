2b373cba33c8dda9d236a32d448490964bcd4854
juandak16 <juand.hidalgo@unet.edu.ve>
2016-12-02 12:42:56 -0400

    Update boardStatus.h

--- include/boardStatus.h
+++ include/boardStatus.h
@@ -16,6 +16,7 @@ class boardStatus
         int playedColumn;
         void setBoard(int board[rows][column]);
         void calculatedScore();
+        void print();
         int evaluatePlayer(int x, int y, int player);
         int checkSurroundings(int x, int y, int player);
 };
@@ -38,6 +39,16 @@ void boardStatus::setBoard(int board[rows][column]){
         }
     }
 }
+void boardStatus::print(){
+    int i,j;
+    for(i = 0; i < rows; i++){
+        for(j = 0; j < column; j++){
+            this->board[i][j] = board[i][j];
+            std::cout<<board[i][j]<<" ";
+        }
+        std::cout<<std::endl;
+    }
+}
 
 
 void boardStatus::calculatedScore(){
@@ -57,6 +68,108 @@ void boardStatus::calculatedScore(){
 }
 
 int boardStatus::checkSurroundings(int x, int j, int player){
-
+    int cont=0,score=0;
+    //vertical
+    for(int i=1;i<=3;i++){
+        if(board[x+i][j]!=player && board[x+i][j]!=0 || board[x-1][j]!=player && board[x-1][j]!=0) break;
+        if(board[x+i][j]==player && x+i<= rows-1){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;
+    //Horizontal derecha
+    for(int i=1;i<=3;i++){
+        if(board[x][j+i]!=player && board[x][j+i]!=0) break;
+        if(board[x][j+i]==player && j+i<= column-1){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;
+    //Horizontal izquierda
+    for(int i=1;i<=3;i++){
+        if(board[x][j-i]!=player && board[x][j-i]!=0) break;
+        if(board[x][j-i]==player && j-i >=0){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;
+    //Diagonal arriba derecha
+    for(int i=1;i<=3;i++){
+        if(board[x-i][j+i]!=player && board[x-i][j+i]!=0) break;
+        if(board[x-i][j+i]==player && x-i>=0 && j+i<=column-1){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;
+    //Diagonal arriba izquierda
+    for(int i=1;i<=3;i++){
+        if(board[x-i][j-i]!=player && board[x-i][j-i]!=0) break;
+        if(board[x-i][j-i]==player && x-i>=0 && j-i>=0){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;        cont=0;
+    //Diagonal abajo izquierda
+    for(int i=1;i<=3;i++){
+        if(board[x+i][j-i]!=player && board[x+i][j-i]!=0) break;
+        if(board[x+i][j-i]==player && x+i<=rows-1 && j-i>=0){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;
+    //Diagonal abajo derecha
+    for(int i=1;i<=3;i++){
+        if(board[x+i][j+i]!=player && board[x+i][j+i]!=0) break;
+        if(board[x+i][j+i]==player && x+i<=rows-1 && j+i<=column-1){
+            cont++;
+        }
+    }
+    if(cont==3){
+        score+=100;
+    }
+    if(cont==2){
+        score+=50;
+    }
+    cont=0;
+    print();
+    std::cout<< x <<" , "<< j <<" = "<< score <<std::endl;
+    std::cin.get();
+    return score;
 }
 #endif
